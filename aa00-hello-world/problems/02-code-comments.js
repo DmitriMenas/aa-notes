663b25c173f8efa10ff2df3b8fbd344f3c3d8d1f
DmitriMenas <dmitri.menas@gmail.com>
2024-06-08 16:07:30 -0700

    hello-world

---
+++ aa00-hello-world/problems/02-code-comments.js
@@ -0,0 +1,27 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log("Welcome to App Academy!");
+
+/* Print more messages to the terminal using console.log */
+
+// 2. This is a single line comment.
+// Try adding your own single line comments:
+
+// My Comment is This!
+
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+
+// Your code here
+/*
+And here is:
+My Multi-line
+comment
+*/
